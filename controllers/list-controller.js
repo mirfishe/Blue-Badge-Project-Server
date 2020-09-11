a91616a1c6fdb3d2463242cb9aa4c08346feec22
Turner Jimerson <turnerj344@gmail.com>
2020-09-11 07:31:50 -0700

    start of my endpoints

--- controllers/list-controller.js
+++ controllers/list-controller.js
@@ -1,45 +1,62 @@
-const router = require('express').Router();
-const List = require('../db').import('../models/list');
-const validateSession = require('../middleware/validate-session');
+const router = require("express").Router();
+const List = require("../db").import("../models/list");
+const validateSession = require("../middleware/validate-session");
 
+/******************
+ ***** Get List *****
+ *******************/
+router.get("/", (req, res) => {
+  List.findAll()
+    .then((list) => res.status(200).json(list))
+    .catch((err) =>
+      res.status(500).json({
+        error: err,
+      })
+    );
+});
+
+router.get("/:id", (req, res) => {
+  List.findOne({ where: { listName: req.params.list.id } })
+    .then((list) => res.status(200).json(list))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 /*******************
  *** Create Entry ****
  ********************/
 router.post("/add", validateSession, (req, res) => {
-    const newList = {
-      listName: req.body.list.title,
-      userID: req.user.id,
-    };
-    List.create(newList)
-      .then((list) => res.status(200).json(list))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
-
+  const newList = {
+    listName: req.body.list.title,
+    userID: req.user.id,
+  };
+  List.create(newList)
+    .then((list) => res.status(200).json(list))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 /***************************
  ******* Update List *******
  ***************************/
 router.put("/update/:id", validateSession, (req, res) => {
-    const updateList = {
-      listName: req.body.list.title
-    };
-    const query = { where: { id: req.params.id, userID: req.user.id } };
-  
-    List.update(updateList, query)
-      .then((list) => res.status(200).json(list))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
+  const updateList = {
+    listName: req.body.list.title,
+  };
+  const query = { where: { id: req.params.id, userID: req.user.id } };
+
+  List.update(updateList, query)
+    .then((list) => res.status(200).json(list))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 /***************************
  ******* Delete List *******
  ***************************/
 router.delete("/delete/:id", validateSession, (req, res) => {
-    const query = { where: { id: req.params.id, userID: req.user.id } };
-  
-    List.destroy(query)
-      .then(() => res.status(200).send("List Deleted"))
-      .catch((err) => res.status(500).json({ error: err }));
-  });
-  
-  module.exports = router;
+  const query = { where: { id: req.params.id, userID: req.user.id } };
+
+  List.destroy(query)
+    .then(() => res.status(200).send("List Deleted"))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
