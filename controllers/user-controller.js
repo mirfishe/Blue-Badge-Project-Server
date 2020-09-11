94ad4fae2af07e589844487e230b88cc4a5dec3b
Michael Fisher <mirfishe@gmail.com>
2020-09-11 10:47:27 -0400

    fixing uncommented not working code in user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,13 +28,40 @@ router.post('/register', function(req, res) {
     .catch(err => res.status(500).json({error: err}))
 
     // Create a default list for the user upon successfully creating user with the userID
-    const newList = {
-        listName: 'Default List',
-        userID: req.user.id,
-      };
-      List.create(newList)
-        .then((list) => res.status(200).json(list))
-        .catch((err) => res.status(500).json({ error: err }));
+    // const newList = {
+    //     listName: 'Default List',
+    //     userID: req.user.id,
+    //   };
+    //   List.create(newList)
+    //     .then((list) => res.status(200).json(list))
+    //     .catch((err) => res.status(500).json({ error: err }));
+
+
+
+    // https://stackoverflow.com/questions/48376479/executing-multiple-sequelize-js-model-query-methods-with-promises-node
+    // const userRegister = User.create({
+    //         email:   req.body.user.email,
+    //         password:   bcrypt.hashSync(req.body.user.password)
+    // });
+
+    // const createDefaultList = List.create({
+    //     listName: 'Default List',
+    //     userID: req.user.id,
+    // });
+
+
+    // Promise
+    // .all([userRegister, createDefaultList])
+    // .then(responses => {
+    //     console.log('**********COMPLETE RESULTS****************');
+    //     console.log(responses[0]); // user profile
+    //     console.log(responses[1]); // all reports
+    // })
+    // .catch(err => {
+    //     console.log('**********ERROR RESULT****************');
+    //     console.log(err);
+    // });
+
 
 });
 
